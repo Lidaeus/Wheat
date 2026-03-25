58b2d5280667276cd48b0496153df5e7d6065bae
Ceres-Architect <ceres-architect@local>
2026-03-25 05:44:58 -0700

    Autoresearch: Set strategy.py to best strategy (2_Inverse_RMSE)

--- autoresearch_sandbox/strategy.py
+++ autoresearch_sandbox/strategy.py
@@ -1,11 +1,13 @@
 import numpy as np
 
 def calculate_loss(sim_yield, obs_yield, sim_lai, obs_lai):
-    nrmse_y = np.sqrt(np.mean((sim_yield - obs_yield)**2)) / (np.mean(obs_yield) + 1e-8)
-    nrmse_l = np.sqrt(np.mean((sim_lai - obs_lai)**2)) / (np.mean(obs_lai) + 1e-8)
+    """
+    Strategy: 2_Inverse_RMSE (Best performing strategy from Autoresearch evolution)
+    """
+    rmse_yield_base = np.sqrt(np.mean(obs_yield**2)) + 1e-8
+    rmse_lai_base = np.sqrt(np.mean(obs_lai**2)) + 1e-8
     
-    # Scalarize pareto logic: distance to origin (0,0) + penalty for imbalance
-    distance = np.sqrt(nrmse_y**2 + nrmse_l**2)
-    imbalance = np.abs(nrmse_y - nrmse_l)
+    rmse_y = np.sqrt(np.mean((sim_yield - obs_yield)**2))
+    rmse_l = np.sqrt(np.mean((sim_lai - obs_lai)**2))
     
-    return distance + 0.5 * imbalance
+    return (rmse_y / rmse_yield_base) + (rmse_l / rmse_lai_base)
